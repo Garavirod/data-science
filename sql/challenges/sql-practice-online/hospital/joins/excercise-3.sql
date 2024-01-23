d7f845e1d8236f1f18fd13e74b0f54c3cdf958f1
Garavirod <rodrigogarciaavila26@gmail.com>
2024-01-22 19:55:59 -0600

    feat(sql): add missing solution exercise 3 join

--- sql/challenges/sql-practice-online/hospital/joins/excercise-3.sql
+++ sql/challenges/sql-practice-online/hospital/joins/excercise-3.sql
@@ -15,3 +15,15 @@ inner join number_doc_addmissions DA on D.doctor_id = DA.attending_doctor_id
 select D.first_name, D.last_name, count(*) as total from admissions as A
 inner join doctors D on A.attending_doctor_id = D.doctor_id
 group by attending_doctor_id
+
+-- Solution 3
+SELECT
+  first_name,
+  last_name,
+  count(*)
+from
+  doctors p,
+  admissions a
+where
+  a.attending_doctor_id = p.doctor_id
+group by p.doctor_id;
