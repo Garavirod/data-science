1a391910d6a242c9597b02e52fa2fd10b0b115af
Garavirod <rodrigogarciaavila26@gmail.com>
2024-03-27 18:46:47 -0600

    feat(data-engineering): fixing issue on docker compose file and main misspellings

--- data-engineering/aws/unstructured-data-proccessing/main.py
+++ data-engineering/aws/unstructured-data-proccessing/main.py
@@ -10,8 +10,8 @@ def define_udfs():
         'extract_file_name_udf': udf(extract_file_name, StringType()),
         'extract_position_udf': udf(extract_position, StringType()),
         'extract_salary_udf': udf(extract_salary, StructType([
-            StructField('salary_start',DoubleType(),True),
-            StructField('salary_end',DoubleType(),True),
+            StructField('salary_start', DoubleType(), True),
+            StructField('salary_end', DoubleType(), True),
         ])),
         'extract_date_udf': udf(extract_start_date, DateType()),
         'extract_end_date_udf': udf(extract_end_date, DateType()),
@@ -23,58 +23,56 @@ def define_udfs():
         'extract_experience_length_udf': udf(extract_experience_length, StringType()),
         'extract_education_length_udf': udf(extract_eduction_length, StringType()),
         'extract_application_location_udf': udf(extract_application_location, StringType()),
-       
+
     }
 
-if __name__ == '__main__':
-    spark = (SparkSession.builder
-             .appName('AWS_Spark_Unstructured')
-             .config(
-                 'spark.jars.packages',
-                 'org.apache.hadoop:hadoop-aws:3.3.1,'
-                 'com.amazonaws:aws-java-sdk:1.11.469')
-             .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
-             .config('spark.hadoop.fs.s3a.access.key', configuration.get('AWS_ACCESS_KEY'))
-             .config('spark.hadoop.fs.s3a.secret.key', configuration.get('AWS_SECRET_KEY'))
-             .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
-             .getOrCreate()
-             )
 
-    text_input_dir = 'file://C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_text'
-    csv_input_dir = 'file://C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_csv'
-    json_input_dir = 'file://C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_json'
-    pdf_input_dir = 'file://C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_pdf'
-    video_input_dir = 'file://C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_video'
-    img_input_dir = 'file://C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_img'
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName('AWS_Spark_Unstructured')\
+        .config(
+        'spark.jars.packages',
+        "org.apache.hadoop:hadoop-aws:3.3.1,"
+        "com.amazonaws:aws-java-sdk:1.11.469")\
+        .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')\
+        .config('spark.hadoop.fs.s3a.access.key', configuration.get('AWS_ACCESS_KEY'))\
+        .config('spark.hadoop.fs.s3a.secret.key', configuration.get('AWS_SECRET_KEY'))\
+        .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')\
+        .getOrCreate()
+
+    text_input_dir = 'file:///Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_text'
+    csv_input_dir = 'file:///C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_csv'
+    json_input_dir = 'file:///C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_json'
+    pdf_input_dir = 'file:///C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_pdf'
+    video_input_dir = 'file:///C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_video'
+    img_input_dir = 'file:///C:/Users/user/Documents/data-science/data-engineering/aws/unstructured-data-proccessing/input/input_img'
 
     # Data schema definition
 
     data_schema = StructType([
-        StructField('file_name',StringType(), True),
-        StructField('position',StringType(), True),
-        StructField('class_code',StringType(), True),
-        StructField('salary_start',DoubleType(), True),
-        StructField('salary_end',DoubleType(), True),
-        StructField('start_date',DateType(), True),
-        StructField('end_date',DateType(), True),
-        StructField('req',StringType(), True),
-        StructField('notes',StringType(), True),
-        StructField('duties',StringType(), True),
-        StructField('selection',StringType(), True),
-        StructField('experience_length',StringType(), True),
-        StructField('job_type',StringType(), True),
-        StructField('education_length',StringType(), True),
-        StructField('school_type',StringType(), True),
-        StructField('application_location',StringType(), True),
+        StructField('file_name', StringType(), True),
+        StructField('position', StringType(), True),
+        StructField('class_code', StringType(), True),
+        StructField('salary_start', DoubleType(), True),
+        StructField('salary_end', DoubleType(), True),
+        StructField('start_date', DateType(), True),
+        StructField('end_date', DateType(), True),
+        StructField('req', StringType(), True),
+        StructField('notes', StringType(), True),
+        StructField('duties', StringType(), True),
+        StructField('selection', StringType(), True),
+        StructField('experience_length', StringType(), True),
+        StructField('job_type', StringType(), True),
+        StructField('education_length', StringType(), True),
+        StructField('school_type', StringType(), True),
+        StructField('application_location', StringType(), True),
     ])
-
 
     udf = define_udfs()
 
     job_bulletin_df = (
-        spark.readSream
+        spark.readStream
         .format('text')
-        .option('wholetext','true')
+        .option('wholetext', 'true')
         .load(text_input_dir)
     )
 
